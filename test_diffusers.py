643d1dd6305b24898c540103fbc2d6d9525b30e3
deforum <deforum.art@gmail.com>
2024-08-04 14:32:23 -0700

    add diffusers txt2img/img2img hybrid pipeline

--- test_diffusers.py
+++ test_diffusers.py
@@ -1,17 +1,29 @@
+import os
 import torch
-from diffusers import FluxPipeline
+from flux_pipeline import FluxImg2ImgPipeline
 
-pipe = FluxPipeline.from_pretrained("black-forest-labs/FLUX.1-schnell", torch_dtype=torch.bfloat16)
+pipe = FluxImg2ImgPipeline.from_pretrained("black-forest-labs/FLUX.1-dev", torch_dtype=torch.bfloat16)
 pipe.enable_model_cpu_offload()
 
-prompt = "cat sushi"
-out = pipe(
-    prompt=prompt,
-    guidance_scale=0.,
-    height=768,
-    width=1360,
-    num_inference_steps=4,
-    max_sequence_length=256,
-).images[0]
+max_frames = 100
+output_dir = "output"
+os.makedirs(output_dir, exist_ok=True)
+image = None
 
-out.save("image.png")
+for i in range(max_frames):
+
+    prompt = "cat sushi"
+    out = pipe(
+        image=image,
+        prompt=prompt,
+        guidance_scale=3,
+        strength=0.2,
+        height=512,
+        width=512,
+        num_inference_steps=28,
+        max_sequence_length=256,
+    ).images[0]
+
+    output_path = os.path.join(output_dir, f"generated_image_{i:05d}.jpg")
+    out.save(output_path)
+    image = out
